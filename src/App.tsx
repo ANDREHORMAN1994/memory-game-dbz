c34c65994135c34c5088f1a805cf586473ce7401
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-11-02 22:43:16 -0300

    :sparkles: configurando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="w-full h-full min-h-screen bg-[url('/images/shen.png')] bg-cover bg-no-repeat bg-center">
       <h1 className="text-center">MEMORY GAME</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
